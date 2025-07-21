39c41fcaa814e62ee4d3326aa76f80582ec701d9
niuyunlong <62708109+niuyunlong@users.noreply.github.com>
2025-07-21 20:48:12 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["yc.ocsen.net"];
+pub const RS_PUB_KEY: &str = "PARxR8k3CFJD+sbRjuBpBAdhGgMUEZZ6XuydDMbAMGk=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
